96352c44e9eeeb6a58d4fcb7441bdb87dcbd4a62
Douglas <87586242+NLdouglas@users.noreply.github.com>
2024-06-25 23:39:38 -0300

    condições

---
+++ Vr/var.py
@@ -0,0 +1,9 @@
+nome= input("digite um funcionario: ")
+empresa = input("digite a instituição: ")
+qntd_funcionarios=int(input("Digite um valor: "))
+mensalidade=float(input("digite um salario: "))
+print(nome + " trabalha na empresa" + empresa) 
+print("possui", qntd_funcionarios, "funcionarios")
+print("A media de mensalidade é de: " + str(mensalidade))
+
+
---
+++ condicoes/ifelse.py
@@ -0,0 +1,7 @@
+nome = input("digite um nome: ")
+idade=int(input("digite uma idade: "))
+if idade <= 39:
+    print("passou")
+else:
+    print("nao passou")
+print("fim")
